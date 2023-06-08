0bfef805695c59df0871db789791416907778cd6
hasanisokay <hasanisokay@gmail.com>
2023-06-08 07:55:06 +0600

    added jwt token

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ async function run() {
     
     // jwt
     app.post("/jwt", (req,res)=>{
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1hr'})
+      const userMail = req.body;
+      const token = jwt.sign(userMail, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'10hr'})
       res.send({token}) 
     })
 
@@ -96,142 +96,6 @@ async function run() {
         return
       }
       return res.send({message:"user already exists"})
-    })
-
-    // check if a user is admin or not
-    app.get("/users/admin/:email", verifyJWT, async(req,res)=>{
-      // first level of security is checking verifyJWT token. 
-      const email = req.params.email;
-      // second level of security. checking user email and token email same or not
-      if(req.decoded.email !== email){
-        res.send({admin: false})
-      }
-
-      // checking admin
-      const query = {email: email};
-      const user = await usersCollection.findOne(query);
-      const result = {admin: user?.role ==="admin"}
-      res.send(result);
-
-    })
-    
-    // secure all users route
-    /** 
-     * 0. dont show secure links to every one.
-     * 1. use verifyJWT
-     * 2. use verifyAdmin middleware
-     *  
-    */  
-    
-    // getting all saved users from database 
-    app.get("/users", verifyJWT, async(req,res)=>{
-      const result = await usersCollection.find().toArray()
-      res.send(result)
-    })
-
-    // update users role
-    app.patch("/users/admin/:id", async(req,res)=>{
-      const id = req.params.id;
-      // console.log(id);
-      const filter = { _id: new ObjectId(id)};
-      const updateDoc = {
-        $set : {
-          role:"admin"
-        },
-      }
-      const result = await usersCollection.updateOne(filter,updateDoc);
-      res.send(result);
-    })
-    // menu api
-    app.get("/menu", async(req,res)=>{
-        const result = await menuCollection.find().toArray()
-        res.send(result)
-    })
-
-    // posting data to db from admin.
-    app.post("/menu", verifyJWT,verifyAdmin, async(req,res)=>{
-      const newItem = req.body;
-      // console.log(newItem);
-      const result = await menuCollection.insertOne(newItem);
-      res.send(result)
-
-
-    })
-    // delete a menu item action from admin
-    app.delete("/menu/:id", verifyJWT,verifyAdmin, async(req,res)=>{
-      const id = req.params.id;
-      // console.log(id);
-      const query = {_id: new ObjectId(id)}
-      const result = await menuCollection.deleteOne(query)
-      res.send(result)
-    })
-
-    // reviews api
-    app.get("/reviews", async(req,res)=>{
-        const result = await reviewsCollection.find().toArray()
-        res.send(result)
-    })
-    // cart collection 
-    app.post("/carts", async(req,res)=>{
-      const item = req.body;
-      const result = await cartCollection.insertOne(item)
-      res.send(result)
-    })
-    app.get("/carts", verifyJWT, async(req,res)=>{
-      const decodedEmail = req.decoded.email 
-      const email = req.query.email;
-      // this if is checking the user is not getting any others information by their email. its checking email in the token and email in the query of api call. if both matched this will return data. else it will give 403
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: "forbidden access"})
-      }
-      
-      if(!email){
-        res.send([])
-      }
-      const query = {email: email};
-      const result = await cartCollection.find(query).toArray()
-      res.send(result)
-
-    })
-    app.delete("/carts/:id", async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await cartCollection.deleteOne(query);
-      res.send(result)
-    })
-    // create payment intent
-    app.post("/create-payment-intent", async(req,res)=>{
-      const {body} = req.body;
-      const amount = price*100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types:["card"]
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret
-      })
-    })
-    // payment related api
-    app.post('/payments', async(req,res)=>{
-      const payment = req.body;
-      const result = await paymentCollection.insertOne(payment)
-      res.send(result)
-    })
-    app.get("/admin-stats", verifyJWT,verifyAdmin, async(req,res)=>{
-      const users = await usersCollection.estimatedDocumentCount()
-      const products = await menuCollection.estimatedDocumentCount()
-      const orders = await paymentCollection.estimatedDocumentCount()
-      
-      const revenue = await paymentCollection.aggregate([
-        {
-          $group: {
-            _id: null,
-            total: {$sum: '$price'}
-          }
-        }
-      ]).toArray() 
-      res.send({users, products, orders, payments: revenue})
     })
 
 
